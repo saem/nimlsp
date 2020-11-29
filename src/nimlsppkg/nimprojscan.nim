31057630a38e9493d04c2ca58a5293603797a9bf
Saem Ghani <saemghani+github@gmail.com>
2020-11-28 17:59:51 -0800

    parse json nimble dump

--- src/nimlsppkg/nimprojscan.nim
+++ src/nimlsppkg/nimprojscan.nim
@@ -1,10 +1,21 @@
 from uri import parseUri, decodeUrl
 from strutils import toHex, startsWith, `%`
-from os import normalizedPath, walkDir, PathComponent, splitFile
+from os import `/`, normalizedPath, walkDir, PathComponent, splitFile, findExe
+from osproc import execCmdEx
+from json import parseJson, `{}`, `getStr`
 
 type
+  NimbleTask = object
+    nimbleUri*: string
+    name*: string
+    help*: string
   NimbleFile = object
     uri*: string
+    name*: string
+    srcDir*: string
+    binDir*: string
+    backend*: string
+    tasks*: seq[NimbleTask]
     # add other nimble relevant things here
   FindKind {.pure.} = enum
     fkNimble, fkLoL # LoL - because it could be anything :D
@@ -17,11 +28,14 @@ type
     dirs*: seq[string]
     ignoredDirs*: seq[string]
     otherFiles*: seq[string]
+    nimbleExe*: string
   UriParseError* = object of Defect
     uri: string
   MoreThanOneNimble* = object of CatchableError
     uri: string
     existingNimble: string
+  NimbleExeNotFound* = object of CatchableError
+  NimbleDumpFailed* = object of CatchableError
 
 proc uriToPath(uri: string): string =
   ## Convert an RFC 8089 file URI to a native, platform-specific, absolute path.
@@ -63,7 +77,7 @@ proc pathToUri(path: string, scheme: string = "file"): string =
 proc `$`(f: DirFind): string =
   result = ""
   result.add "uri: " & f.uri & "\n"
-  result.add "nimble file: " & f.nimble.uri & "\n"
+  result.add "nimble file: " & $f.nimble & "\n"
   result.add "nims files: " & $f.nimscripts & "\n"
   result.add "cfg files: " & $f.cfgs & "\n"
   result.add "nim files: " & $f.nims & "\n"
@@ -71,8 +85,11 @@ proc `$`(f: DirFind): string =
   result.add "other files: " & $f.otherFiles & "\n"
   result.add "ignored directories: " & $f.ignoredDirs & "\n"
 
+proc isNimbleProject(f: DirFind): bool =
+  result = f.nimble.uri.len > 0
+
 proc doFind(uri: string): owned DirFind =
-  result = DirFind(uri: uri)
+  result = DirFind(uri: uri, nimbleExe: findExe("nimble"))
   for f in walkDir(result.uri.uriToPath):
     let
       fUri = f.path.pathToUri
@@ -99,14 +116,37 @@ proc doFind(uri: string): owned DirFind =
       of ".nim": result.nims.add(fUri)
       of ".cfg": result.cfgs.add(fUri)
       else: result.otherFiles.add(fUri)
+  
+  if result.isNimbleProject:
+    let
+      nimbleExe = result.nimbleExe
+      path = result.uri.uriToPath
+    if nimbleExe == "":
+      raise newException(NimbleExeNotFound, "Did not find nimble executable")
+    var
+      d = execCmdEx(nimbleExe & " dump --json", workingDir = uri.uriToPath)
+      j = d.output.parseJson
+    
+    if d.exitCode != 0:
+      raise newException(NimbleDumpFailed,
+        "Nimble dump exited with code: " & $d.exitCode)
+
+    result.nimble.name = j{"name"}.getStr
+    result.nimble.srcDir = path / j{"srcDir"}.getStr
+    result.nimble.binDir = path / j{"binDir"}.getStr
+    result.nimble.backend = j{"backend"}.getStr
 
 when isMainModule:
-  from os import `/`, parentDir
+  from os import parentDir
 
   const
     rootUri = parentDir(parentDir(parentDir(currentSourcePath()))).pathToUri
 
   var
     find = doFind(rootUri)
+    nimbleDump = execCmdEx("nimble")
 
   echo $find
+  
+  if find.isNimbleProject:
+    echo "It's a nimble project"
