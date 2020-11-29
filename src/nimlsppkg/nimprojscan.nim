f2157e37f98abc917eeb641cd0c5e7996a4e5b89
Saem Ghani <saemghani+github@gmail.com>
2020-11-28 18:26:21 -0800

    Convert strings to actual Uri

--- src/nimlsppkg/nimprojscan.nim
+++ src/nimlsppkg/nimprojscan.nim
@@ -1,4 +1,4 @@
-from uri import parseUri, decodeUrl
+from uri import Uri, parseUri, decodeUrl, `/`, `$`
 from strutils import toHex, startsWith, `%`
 from os import `/`, normalizedPath, walkDir, PathComponent, splitFile, findExe
 from osproc import execCmdEx
@@ -6,77 +6,80 @@ from json import parseJson, `{}`, `getStr`
 
 type
   NimbleTask = object
-    nimbleUri*: string
+    nimbleUri*: Uri
     name*: string
     help*: string
   NimbleFile = object
-    uri*: string
+    uri*: Uri
     name*: string
-    srcDir*: string
-    binDir*: string
+    srcDir*: Uri
+    binDir*: Uri
     backend*: string
     tasks*: seq[NimbleTask]
     # add other nimble relevant things here
   FindKind {.pure.} = enum
     fkNimble, fkLoL # LoL - because it could be anything :D
   DirFind = object
-    uri*: string
+    uri*: Uri
     nimble*: NimbleFile
-    cfgs*: seq[string]
-    nimscripts*: seq[string]
-    nims*: seq[string]
-    dirs*: seq[string]
-    ignoredDirs*: seq[string]
-    otherFiles*: seq[string]
+    cfgs*: seq[Uri]
+    nimscripts*: seq[Uri]
+    nims*: seq[Uri]
+    dirs*: seq[Uri]
+    ignoredDirs*: seq[Uri]
+    otherFiles*: seq[Uri]
     nimbleExe*: string
   UriParseError* = object of Defect
-    uri: string
+    uri: Uri
   MoreThanOneNimble* = object of CatchableError
-    uri: string
-    existingNimble: string
+    uri: Uri
+    existingNimble: Uri
   NimbleExeNotFound* = object of CatchableError
   NimbleDumpFailed* = object of CatchableError
 
-proc uriToPath(uri: string): string =
+proc uriToPath(uri: Uri): string =
   ## Convert an RFC 8089 file URI to a native, platform-specific, absolute path.
   #let startIdx = when defined(windows): 8 else: 7
   #normalizedPath(uri[startIdx..^1])
-  let parsed = uri.parseUri
-  if parsed.scheme != "file":
-    var e = newException(UriParseError, "Invalid scheme: " & parsed.scheme & ", only \"file\" is supported")
+  if uri.scheme != "file":
+    var e = newException(UriParseError,
+      "Invalid scheme: $1, only \"file\" supported" % [uri.scheme])
     e.uri = uri
     raise e
-  if parsed.hostname != "":
-    var e = newException(UriParseError, "Invalid hostname: " & parsed.hostname & ", only empty hostname is supported")
+  if uri.hostname != "":
+    var e = newException(UriParseError, 
+      "Invalid hostname: $1, only empty hostname supported" % [uri.hostname])
     e.uri = uri
     raise e
   return normalizedPath(
     when defined(windows):
-      parsed.path[1..^1]
+      uri.path[1..^1]
     else:
-      parsed.path).decodeUrl
+      uri.path).decodeUrl
 
-proc pathToUri(path: string, scheme: string = "file"): string =
+proc pathToUri(path: string, scheme: string = "file"): Uri =
   ## This is a modified copy of encodeUrl in the uri module. This doesn't encode
   ## the / character, meaning a full file path can be passed in without breaking
   ## it.
-  let
-    schemeLen = scheme.len + 3 # 3 is for ://
-    assumedNonAlnumChars = path.len shr 2 # assume 12% non-alnum-chars
-  result = newStringOfCap(schemeLen + path.len + assumedNonAlnumChars)
-  result.add scheme
-  result.add "://"
-  for c in path:
-    case c
-    # https://tools.ietf.org/html/rfc3986#section-2.3
-    of 'a'..'z', 'A'..'Z', '0'..'9', '-', '.', '_', '~', '/': add(result, c)
-    else:
-      add(result, '%')
-      add(result, toHex(ord(c), 2))
+  # let
+  #   schemeLen = scheme.len + 3 # 3 is for ://
+  #   assumedNonAlnumChars = path.len shr 2 # assume 12% non-alnum-chars
+  # var str = newStringOfCap(schemeLen + path.len + assumedNonAlnumChars)
+  # str.add scheme
+  # str.add "://"
+  # for c in path:
+  #   case c
+  #   # https://tools.ietf.org/html/rfc3986#section-2.3
+  #   of 'a'..'z', 'A'..'Z', '0'..'9', '-', '.', '_', '~', '/': add(str, c)
+  #   else:
+  #     add(str, '%')
+  #     add(str, toHex(ord(c), 2))
+  result = parseUri(path)
+  result.scheme = scheme
 
 proc `$`(f: DirFind): string =
   result = ""
-  result.add "uri: " & f.uri & "\n"
+  result.add "uri: " & $f.uri & "\n"
   result.add "nimble file: " & $f.nimble & "\n"
   result.add "nims files: " & $f.nimscripts & "\n"
   result.add "cfg files: " & $f.cfgs & "\n"
@@ -86,9 +89,9 @@ proc `$`(f: DirFind): string =
   result.add "ignored directories: " & $f.ignoredDirs & "\n"
 
 proc isNimbleProject(f: DirFind): bool =
-  result = f.nimble.uri.len > 0
+  result = f.nimble.uri.path.len > 0
 
-proc doFind(uri: string): owned DirFind =
+proc doFind(uri: Uri): owned DirFind =
   result = DirFind(uri: uri, nimbleExe: findExe("nimble"))
   for f in walkDir(result.uri.uriToPath):
     let
@@ -103,12 +106,12 @@ proc doFind(uri: string): owned DirFind =
     of pcFile, pcLinkToFile:
       case ext
       of ".nimble":
-        if result.nimble.uri.len == 0:
+        if result.nimble.uri.path.len == 0:
           result.nimble.uri = fUri
         else:
           var e = newException(MoreThanOneNimble,
             "Have $1, also got $2, max one per folder" %
-              [result.nimble.uri, fUri])
+              [$result.nimble.uri, $fUri])
           e.uri = fUri
           e.existingNimble = result.nimble.uri
           raise e
@@ -132,8 +135,8 @@ proc doFind(uri: string): owned DirFind =
         "Nimble dump exited with code: " & $d.exitCode)
 
     result.nimble.name = j{"name"}.getStr
-    result.nimble.srcDir = path / j{"srcDir"}.getStr
-    result.nimble.binDir = path / j{"binDir"}.getStr
+    result.nimble.srcDir = (path / j{"srcDir"}.getStr).pathToUri
+    result.nimble.binDir = (path / j{"binDir"}.getStr).pathToUri
     result.nimble.backend = j{"backend"}.getStr
 
 when isMainModule:
