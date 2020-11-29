4f40390baaa8d1a6b11274de2fe081a918e75941
Saem Ghani <saemghani+github@gmail.com>
2020-11-29 13:37:27 -0800

    Part way to work through file path to URI

--- src/nimlsppkg/nimprojscan.nim
+++ src/nimlsppkg/nimprojscan.nim
@@ -1,5 +1,6 @@
-from uri import Uri, parseUri, decodeUrl, `/`, `$`
-from strutils import toHex, startsWith, `%`
+from uri import Uri, parseUri, encodeUrl, decodeUrl, `/`, `$`
+from strutils import toHex, startsWith, `%`, replace, find, split
+from sequtils import mapIt, filterIt
 from os import `/`, normalizedPath, walkDir, PathComponent, splitFile, findExe
 from osproc import execCmdEx
 from json import parseJson, `{}`, `getStr`
@@ -37,6 +38,15 @@ type
   NimbleExeNotFound* = object of CatchableError
   NimbleDumpFailed* = object of CatchableError
 
+proc authority(uri: Uri): string =
+  let
+    username = if uri.username.len > 0: uri.username else: ""
+    password = if uri.password.len > 0: ":$1" % [uri.password] else: ""
+    userinfo = if username.len > 0: username & password & "@" else: ""
+    port = if uri.port.len > 0: ":" & uri.port else: ""
+  
+  result = if uri.hostname.len > 0: userinfo & uri.hostname & port else: ""
+
 proc uriToPath(uri: Uri): string =
   ## Convert an RFC 8089 file URI to a native, platform-specific, absolute path.
   #let startIdx = when defined(windows): 8 else: 7
@@ -51,31 +61,97 @@ proc uriToPath(uri: Uri): string =
       "Invalid hostname: $1, only empty hostname supported" % [uri.hostname])
     e.uri = uri
     raise e
-  return normalizedPath(
-    when defined(windows):
-      uri.path[1..^1]
-    else:
-      uri.path).decodeUrl
+  result = (
+    when defined(windows): uri.path[1..^1]
+    else: uri.path).decodeUrl
+  when defined(windows):
+    result = result.replace("/", "\\")
 
-proc pathToUri(path: string, scheme: string = "file"): Uri =
+proc pathToUri(absolutePath: string): Uri =
+  ## Based on vscode uri module: https://github.com/microsoft/vscode-uri/blob/master/src/index.ts#L752
+  ## 
+  ## Assumes absolute paths
+  ## 
+  ## Old Docs:
   ## This is a modified copy of encodeUrl in the uri module. This doesn't encode
-  ## the / character, meaning a full file path can be passed in without breaking
+  ## the / character, meaning a full file absolutePath can be passed in without breaking
   ## it.
   # let
   #   schemeLen = scheme.len + 3 # 3 is for ://
-  #   assumedNonAlnumChars = path.len shr 2 # assume 12% non-alnum-chars
-  # var str = newStringOfCap(schemeLen + path.len + assumedNonAlnumChars)
+  #   assumedNonAlnumChars = absolutePath.len shr 2 # assume 12% non-alnum-chars
+  # var str = newStringOfCap(schemeLen + absolutePath.len + assumedNonAlnumChars)
   # str.add scheme
   # str.add "://"
-  # for c in path:
+  # for c in absolutePath:
   #   case c
   #   # https://tools.ietf.org/html/rfc3986#section-2.3
   #   of 'a'..'z', 'A'..'Z', '0'..'9', '-', '.', '_', '~', '/': add(str, c)
   #   else:
   #     add(str, '%')
   #     add(str, toHex(ord(c), 2))
-  result = parseUri(path)
-  result.scheme = scheme
+
+  when defined(windows):
+    let
+      fwdSlashPath = absolutePath.normalizedPath().replace("\\", "/")
+      isUnc = fwdSlashPath.startsWith("//")
+      fsPath = if isUnc: fwdSlashPath[1..^1] else: fwdSlashPath
+      authorityEnd = if isUnc: fsPath.find('/', 1) : 0
+      rawAuthority = if authorityEnd > 0: fsPath[1..^authorityEnd] else: ""
+      rawPath = if isUnc: fsPath.substr(authorityEnd)
+                elif fsPath.startsWith('/'): fsPath
+                else: "/" & fsPath
+      path = rawPath.split('/').filterIt(it.len > 0).mapIt(it.encodeUrl())
+      (user, pass, host, port) = rawAuthority.split("@", 1).mapIt(case it.len
+          of 0: ("", "")
+          of 1: ("", it[0])
+          of 2: (it[0], it[1])
+        ).mapIt(block:
+          let
+            userparts = it[0].split(':', 1)
+            hostparts = it[1].split(':', 1)
+            user = userparts[0]
+            pass = if userparts.len > 1: userparts[1] : ""
+            host = hostparts[0]
+            port = if hostparts.len > 1: hostparts[1] : ""
+          (user.encodeUrl, pass.encodeUrl, host.encodeUrl, port)
+        )
+  else:
+    fsPath = absolutePath.normalizedPath
+    path = fsPath.split("/")
+  result = initUri()
+  uri.scheme = "file"
+  uri.username = user
+  uri.password = pass
+  uri.hostname = host
+  uri.port = port
+  uri.path = "/" & path.join("/")
+
+  let
+    fsPath = when defined(windows): absolutePath.replace("\\", "/")
+             else: absolutePath
+    hasUnc = fsPath.startsWith("//")
+    authority = if fsPath.startsWith("//"):
+  
+  let p = when defined(windows):
+      if path[1] == ':' and (path[0] in {'a'..'z'} or path[0] in {'A'..'Z'}):
+        "/" & path.replace("\\", "/")
+      else:
+        path.replace("\\", "/")
+    else:
+      path
+  
+  result = parseUri(p)
+  
+  result.path = if p.startsWith("//"):
+      let
+        i = p.find('/', 2)
+        pathPart = p[i .. ^1]
+      if i == -1 or pathPart == "": "/"
+      else: pathPart
+    else:
+      p
+
+  result.scheme = "file"
 
 proc `$`(f: DirFind): string =
   result = ""
@@ -141,6 +217,7 @@ proc doFind(uri: Uri): owned DirFind =
 
 when isMainModule:
   from os import parentDir
+  from uri import isAbsolute, initUri
 
   const
     rootUri = parentDir(parentDir(parentDir(currentSourcePath()))).pathToUri
@@ -153,3 +230,8 @@ when isMainModule:
   
   if find.isNimbleProject:
     echo "It's a nimble project"
+  
+  for s in ["/test/butts", "file:///test/butts"]:
+    let u = parseUri(s)
+    echo "Parsed out scheme: $1, hostname: $2, authority: $3, path: $4" %
+      [u.scheme, u.hostname, u.authority, u.path]
